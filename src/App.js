ea09fe225f2bf6f02a08b66044c92fd512ac12fe
Sid Ahrend <sidahrend57@gmail.com>
2019-10-22 16:13:08 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Currency from './Components/CurrencyConverter';
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <h2> Render Props </h2>
+        <Currency />
       </div>
     );
   }
 }
 
 export default App;
+
